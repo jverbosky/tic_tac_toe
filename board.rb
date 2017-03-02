34336074b36d067f1f1a005f0fead419fe945ee5
jverbosk <jverbosk@gmail.com>
2017-03-01 22:13:44 -0500

    updated variable and method names in Board class to improve clarity

--- board.rb
+++ board.rb
@@ -1,32 +1,28 @@
 class Board
 
   # Method to access the variables in the class - attr_ is shorthand syntax for creating getter/setter methods
-  # attr_reader :t_board  # attr_reader = read-only, this will pass test 1, but then will never be able to update the value of board
-  attr_accessor :t_board  # attr_reader = read-only, this will pass test 1, but then will never be able to update the value of board
+  # attr_reader :game_board  # attr_reader = read-only, this will pass test 1, but then will never be able to update the value of board
+  attr_accessor :game_board  # attr_reader = read-only, this will pass test 1, but then will never be able to update the value of board
 
   def initialize()
     # use instance variable if not resetting the value anywhere else
-    @t_board = ["", "", "", "", "", "", "", "", ""]
+    @game_board = ["", "", "", "", "", "", "", "", ""]
   end
 
   def to_s()
-    return "The board is: #{@t_board}."
+    return "The board is: #{@game_board}."
   end
 
-  def set_position(position, character)
-    @t_board[position] = character
+  def position_open?(position)
+    @game_board[position] == ""
   end
 
-  def check_position?(position)
-    if @t_board[position] == ""
-      true
-    else
-      false
-    end
+  def set_position(position, mark)
+    @game_board[position] = mark if position_open?(position)
   end
 
-  def check_full?()
-    @t_board.count("") == 0
+  def board_full?()
+    @game_board.count("") == 0
   end
 
 end
--- test_board.rb
+++ test_board.rb
@@ -6,14 +6,14 @@ class TestTicTacToe < Minitest::Test
   def test_1_verify_board_array_exists
     board = Board.new
     result = Array.new(9, "")
-    assert_equal(result, board.t_board)
+    assert_equal(result, board.game_board)
   end
 
   def test_2_verify_update_X_at_1st
     board = Board.new
     board.set_position(0, "X")
     result = ["X", "", "", "", "", "", "", "", ""]
-    assert_equal(result, board.t_board)
+    assert_equal(result, board.game_board)
   end
 
   def test_3_verify_update_O_at_5th_X_at_1st
@@ -21,42 +21,42 @@ class TestTicTacToe < Minitest::Test
     board.set_position(0, "X")
     board.set_position(4, "O")
     result = ["X", "", "", "", "O", "", "", "", ""]
-    assert_equal(result, board.t_board)
+    assert_equal(result, board.game_board)
   end
 
   def test_4_verify_update_X_at_8th_O_at_5th_X_at_1st
     board = Board.new
-    board.t_board = ["X", "", "", "", "O", "", "", "", ""]
+    board.game_board = ["X", "", "", "", "O", "", "", "", ""]
     board.set_position(7, "X")
     result = ["X", "", "", "", "O", "", "", "X", ""]
-    assert_equal(result, board.t_board)
+    assert_equal(result, board.game_board)
   end
 
   def test_5_verify_spot_already_taken
     board = Board.new
-    board.t_board = ["X", "", "", "", "O", "", "", "", ""]
-    result = board.check_position?(4)
+    board.game_board = ["X", "", "", "", "O", "", "", "", ""]
+    result = board.position_open?(4)
     assert_equal(false, result)
   end
 
   def test_6_verify_spot_is_open
     board = Board.new
-    board.t_board = ["X", "", "", "", "O", "", "", "", ""]
-    result = board.check_position?(3)
+    board.game_board = ["X", "", "", "", "O", "", "", "", ""]
+    result = board.position_open?(3)
     assert_equal(true, result)
   end
 
   def test_7_verify_all_spots_full
     board = Board.new
-    board.t_board = ["X", "O", "X", "X", "O", "O", "O", "X", "X"]
-    result = board.check_full?
+    board.game_board = ["X", "O", "X", "X", "O", "O", "O", "X", "X"]
+    result = board.board_full?
     assert_equal(true, result)
   end
 
   def test_8_verify_board_almost_full
     board = Board.new
-    board.t_board = ["X", "O", "", "X", "O", "O", "O", "X", "X"]
-    result = board.check_full?
+    board.game_board = ["X", "O", "", "X", "O", "O", "O", "X", "X"]
+    result = board.board_full?
     assert_equal(false, result)
   end
 
