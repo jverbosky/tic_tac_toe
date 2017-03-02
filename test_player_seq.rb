b8d4ebcac5a4bf3e7d8171ef8be69491c9e90b7b
jverbosk <jverbosk@gmail.com>
2017-03-02 14:44:13 -0500

    wrote first test for PlayerSequential class

--- test_player_seq.rb
+++ test_player_seq.rb
@@ -4,8 +4,11 @@ require_relative "player_seq.rb"
 class TestPlayerSequential < Minitest::Test
 
   def test_1_verify_first_move
-    player = PlayerSequential.new
-    result = player.move
+    board = Board.new
+    p1 = PlayerSequential.new
+    turn = Turn.new
+    round = turn.get_round
+    result = p1.move(round)
     assert_equal("t1", result)
   end
 
