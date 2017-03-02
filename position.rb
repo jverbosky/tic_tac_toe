fcbd93cb04a31630f72818e9103991e9e60615d7
jverbosk <jverbosk@gmail.com>
2017-03-02 12:52:20 -0500

    wrote Position class and get_index method, passing test

--- position.rb
+++ position.rb
@@ -14,3 +14,14 @@
 #    - bottom = b    - right = 3
 #    -----------------------------
 
+class Position
+
+  def initialize()
+    @map = {"t1" => 0, "t2" => 1, "t3" => 2, "m1" => 3, "m2" => 4, "m3" => 5, "b1" => 6, "b2" => 7, "b3" => 8}
+  end
+
+  def get_index(move)
+    @map[move]
+  end
+
+end
