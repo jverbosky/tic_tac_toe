804dc3191d81b53bfe3e7ee47eafb7360605be67
jverbosk <jverbosk@gmail.com>
2017-03-03 18:11:38 -0500

    continuing to test block() method, nothing to block on 1 board layout in round 8, wrote win() method

--- sandbox/3.rb
+++ sandbox/3.rb
@@ -1,103 +1,147 @@
 require_relative "../board.rb"
-
-# round_4 = ["", "", "", "", "", "", "", "", ""]
-# round_4 = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
-
-# All possible board positions after round 4
-
+# --------------------------------------------------------------------
+# board = ["", "", "", "", "", "", "", "", ""]
+# board_index = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+# --------------------------------------------------------------------
+# Board positions after round 4
+# --------------------------------------------------------------------
 # X O -
-# - O -
+# - O -   # variation 1
 # - - X
-
-# round_4 = ["X", "O", "", "", "O", "", "", "", "X"]  # variation 1 - X blocks O at 7
-# round_4 = ["X", "", "", "O", "O", "", "", "", "X"]  # variation 2 - X blocks O at 5
-# round_4 = ["X", "", "", "", "O", "O", "", "", "X"]  # variation 3 - X blocks O at 3
-# round_4 = ["X", "", "", "", "O", "", "", "O", "X"]  # variation 4 - X blocks O at 1
-# round_4 = ["", "O", "X", "", "O", "", "X", "", ""]  # variation 5 - X blocks O at 7
-# round_4 = ["", "", "X", "O", "O", "", "X", "", ""]  # variation 6 - X blocks O at 5
-# round_4 = ["", "", "X", "", "O", "O", "X", "", ""]  # variation 7 - X blocks O at 3
-# round_4 = ["", "", "X", "", "O", "", "X", "O", ""]  # variation 8 - X blocks O at 1
-
+# --------------------------------------------------------------------
+# Block when only 1 potentially winning position
+# round_5 = ["X", "O", "", "", "O", "", "", "", "X"]  # variation 1 - X blocks O at 7
+# round_5 = ["X", "", "", "O", "O", "", "", "", "X"]  # variation 2 - X blocks O at 5
+# round_5 = ["X", "", "", "", "O", "O", "", "", "X"]  # variation 3 - X blocks O at 3
+# round_5 = ["X", "", "", "", "O", "", "", "O", "X"]  # variation 4 - X blocks O at 1
+# round_5 = ["", "O", "X", "", "O", "", "X", "", ""]  # variation 5 - X blocks O at 7
+# round_5 = ["", "", "X", "O", "O", "", "X", "", ""]  # variation 6 - X blocks O at 5
+# round_5 = ["", "", "X", "", "O", "O", "X", "", ""]  # variation 7 - X blocks O at 3
+# round_5 = ["", "", "X", "", "O", "", "X", "O", ""]  # variation 8 - X blocks O at 1
+# --------------------------------------------------------------------
+# Board positions after round 5
+# --------------------------------------------------------------------
 # X O -
-# - O -
+# - O -   # variation 1
 # - X X
-
-# round_5 = ["X", "O", "", "", "O", "", "", "X", "X"]  # variation 1 - O blocks X at 6
-# round_5 = ["X", "", "", "O", "O", "X", "", "", "X"]  # variation 2 - O blocks X at 2
-# round_5 = ["X", "", "", "X", "O", "O", "", "", "X"]  # variation 3 - O blocks X at 6
-# round_5 = ["X", "X", "", "", "O", "", "", "O", "X"]  # variation 4 - O blocks X at 2
-# round_5 = ["", "O", "X", "", "O", "", "X", "X", ""]  # variation 5 - O blocks X at 8
-# round_5 = ["", "", "X", "O", "O", "X", "X", "", ""]  # variation 6 - O blocks X at 8
-# round_5 = ["", "", "X", "X", "O", "O", "X", "", ""]  # variation 7 - O blocks X at 0
-round_5 = ["", "X", "X", "", "O", "", "X", "O", ""]  # variation 8 - O blocks X at 0
-
+# --------------------------------------------------------------------
+# Block when 2 potentially winning positions (1 already taken by O)
+# round_6 = ["X", "O", "", "", "O", "", "", "X", "X"]  # variation 1 - O blocks X at 6
+# round_6 = ["X", "", "", "O", "O", "X", "", "", "X"]  # variation 2 - O blocks X at 2
+# round_6 = ["X", "", "", "X", "O", "O", "", "", "X"]  # variation 3 - O blocks X at 6
+# round_6 = ["X", "X", "", "", "O", "", "", "O", "X"]  # variation 4 - O blocks X at 2
+# round_6 = ["", "O", "X", "", "O", "", "X", "X", ""]  # variation 5 - O blocks X at 8
+# round_6 = ["", "", "X", "O", "O", "X", "X", "", ""]  # variation 6 - O blocks X at 8
+# round_6 = ["", "", "X", "X", "O", "O", "X", "", ""]  # variation 7 - O blocks X at 0
+# round_6 = ["", "X", "X", "", "O", "", "X", "O", ""]  # variation 8 - O blocks X at 0
+# --------------------------------------------------------------------
+# Board positions after round 6
+# --------------------------------------------------------------------
 # X O -
-# - O -
-# - X X
-
-# round_6 = ["X", "O", "", "", "O", "", "O", "X", "X"]  # variation 1
-# round_6 = ["X", "", "O", "O", "O", "X", "", "", "X"]  # variation 2
-# round_6 = ["X", "", "", "X", "O", "O", "O", "", "X"]  # variation 3
-# round_6 = ["X", "X", "O", "", "O", "", "", "O", "X"]  # variation 4
-# round_6 = ["", "O", "X", "", "O", "", "X", "X", "O"]  # variation 5 = O block same as variation 1
-# round_6 = ["", "", "X", "O", "O", "X", "X", "", "O"]  # variation 6 = O block same as variation 2
-# round_6 = ["O", "", "X", "X", "O", "O", "X", "", ""]  # variation 7 = O block same as variation 3
-# round_6 = ["0", "", "X", "", "O", "", "X", "O", ""]  # variation 8 = O block same as variation 4
-
+# - O -   # variation 1
+# O X X
+# --------------------------------------------------------------------
+# Block when 2 potentially winning positions (1 already taken by X)
+# round_7 = ["X", "O", "", "", "O", "", "O", "X", "X"]  # variation 1 - X blocks O at 2
+# round_7 = ["X", "", "O", "O", "O", "X", "", "", "X"]  # variation 2 - X blocks O at 6
+# round_7 = ["X", "", "", "X", "O", "O", "O", "", "X"]  # variation 3 - X blocks O at 2
+# round_7 = ["X", "X", "O", "", "O", "", "", "O", "X"]  # variation 4 - X blocks O at 6
+# round_7 = ["", "O", "X", "", "O", "", "X", "X", "O"]  # variation 5 - X blocks O at 0
+# round_7 = ["", "", "X", "O", "O", "X", "X", "", "O"]  # variation 6 - X blocks O at 0
+# round_7 = ["O", "", "X", "X", "O", "O", "X", "", ""]  # variation 7 - X blocks O at 8
+# round_7 = ["O", "", "X", "", "O", "", "X", "O", ""]  # variation 8 - X blocks O at 1
+# --------------------------------------------------------------------
+# Board positions after round 7
+# --------------------------------------------------------------------
+# X O X                        # O X X
+# - O -   # variation 1        # - O -   # variation 8 (nothing to block)
+# O X X                        # X O -
+# --------------------------------------------------------------------
+# Block when 3 potentially winning positions (2 already taken by O)
+# round_8 = ["X", "O", "X", "", "O", "", "O", "X", "X"]  # variation 1 - O blocks X at 5
+# round_8 = ["X", "", "O", "O", "O", "X", "X", "", "X"]  # variation 2 - O blocks X at 7
+# round_8 = ["X", "", "X", "X", "O", "O", "O", "", "X"]  # variation 3 - O blocks X at 1
+# round_8 = ["X", "X", "O", "", "O", "", "X", "O", "X"]  # variation 4 - O blocks X at 3
+# round_8 = ["X", "O", "X", "", "O", "", "X", "X", "O"]  # variation 5 - O blocks X at 3
+# round_8 = ["X", "", "X", "O", "O", "X", "X", "", "O"]  # variation 6 - O blocks X at 1
+# round_8 = ["O", "", "X", "X", "O", "O", "X", "", "X"]  # variation 7 - O blocks X at 7
+round_8 = ["O", "X", "X", "", "O", "", "X", "O", ""]  # variation 8 - O wins at 8
+# --------------------------------------------------------------------
+# Board positions after round 8
+# --------------------------------------------------------------------
+# X O X                        # O X X
+# - O O   # variation 1        # - O -   # variation 8 (nothing to block)
+# O X X                        # X O O
+# --------------------------------------------------------------------
+# Block when 3 potentially winning positions (2 already taken by O)
+# round_9 = ["X", "O", "X", "", "O", "", "O", "X", "X"]  # variation 1 - X blocks O at 
+# round_9 = ["X", "", "O", "O", "O", "X", "X", "", "X"]  # variation 2 - X blocks O at 
+# round_9 = ["X", "", "X", "X", "O", "O", "O", "", "X"]  # variation 3 - X blocks O at 
+# round_9 = ["X", "X", "O", "", "O", "", "X", "O", "X"]  # variation 4 - X blocks O at 
+# round_9 = ["X", "O", "X", "", "O", "", "X", "X", "O"]  # variation 5 - X blocks O at 
+# round_9 = ["X", "", "X", "O", "O", "X", "X", "", "O"]  # variation 6 - X blocks O at 
+# round_9 = ["O", "", "X", "X", "O", "O", "X", "", "X"]  # variation 7 - X blocks O at 
+# round_9 = ["O", "X", "X", "", "O", "", "X", "O", "O"]  # variation 8 - game over
+# --------------------------------------------------------------------
 
 wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
-def block_o(wins, x_pos, o_pos) # pos of player to block
-  block = []
-  wins.each { |win| block = win - o_pos if (win - o_pos).count == 1 }
-  block
-end
-
 board = Board.new
-board.game_board = round_5
-# print board  # The board is: ["X", "O", "", "", "O", "", "X", "", ""]
+board.game_board = round_8  # change round number to test different board layouts
 
 x_pos = board.get_x
 o_pos = board.get_o
 # p x_pos
 # p o_pos
 
-# Round 5 - block O
-# v1/5 = [7], v2/6 = [5], v3/7 = [3], v4/8 = [1]
-# p block_o(wins, x_pos, o_pos)
+# Method to return position to win, false if none
+def win(wins, winner, loser)
+  position = []
+  wins.each do |win|
+    if (win - winner).count == 1
+      position.push((win - winner)[0]) unless (loser & (win - winner)).count == 1
+    end
+  end
+  position.count > 0 ? position[0] : false
+end
 
+# Method to return position to block, call win() if nothing to block
+def block(wins, blocker, blockee)
+  position = []
+  wins.each do |win|
+    if (win - blockee).count == 1  # if blockee is 1 move away from a win
+      # block the potential winning position, unless blocker already has a mark there
+      position.push((win - blockee)[0]) unless (blocker & (win - blockee)).count == 1
+    end
+  end
+  position.count > 0 ? position[0] : win(wins, blocker, blockee)
+end
 
+p block(wins, o_pos, x_pos)  # O blocking X (round 6 - working, round 8 - working)
+# p block(wins, x_pos, o_pos)  # X blocking O (round 5 - working, round 7 - working, round 9 - )
 
-# ____________________________________________________________________
-#
-# Block X Variation (will likely merge, but here for reference)
-# ____________________________________________________________________
+# --------------------------------------------------------------------
+# Historical Block Variations (here for reference)
+# --------------------------------------------------------------------
+
+# Round 5 - only 1 spot to block
+# def block_o(wins, x_pos, o_pos) # pos of player to block
+#   block = []
+#   wins.each { |win| block = win - o_pos if (win - o_pos).count == 1 }
+#   block
+# end
 
 # Round 6 - block X
-# v1/5 = [4], v2/6 = [], v3/7 = [], v4/8 = []
 # def block_x(wins, o_pos, x_pos)
 #   block = []
 #   wins.each do |win|
 #     if (win - x_pos).count == 1  # if X is 1 move away from a win
-#         block.push((win - x_pos)[0])  # 
+#         block.push((win - x_pos)[0])
 #         block.pop if (o_pos & block).count == 1
 #     end
 #   end
 #   block  # [6]
 # end
-
-# Method to return position to block, false if none
-def block(wins, blocker, blockee)
-  block = []
-  wins.each do |win|
-    if (win - blockee).count == 1  # if blockee is 1 move away from a win
-      # block the potential winning position, unless blocker already has a mark there
-      block.push((win - blockee)[0]) unless (blocker & (win - blockee)).count == 1
-    end
-  end
-  block.count > 0 ? block[0] : false
-end
-
 
 # X O -
 # - O -
@@ -111,18 +155,15 @@ end
 
 # if o_pos & block == block
 
-p block(wins, o_pos, x_pos)  # [4]
-
 # Compare current X position against winning position
 # x_pos = [0, 6]
 # win = [0, 3, 6]
 # block = win - x_pos  # [0, 3, 6] - [0, 6]
 # puts block  # 3
 
-# ____________________________________________________________________
-#
+# --------------------------------------------------------------------
 # Strategy notes
-# ____________________________________________________________________
+# --------------------------------------------------------------------
 
 # X
 # ____________________________
