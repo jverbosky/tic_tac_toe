80626c539b0d89de9f43d2121269fe8b5d6f4b67
jverbosk <jverbosk@gmail.com>
2017-03-02 23:39:45 -0500

    added comments and optional output options to main.rb

--- main.rb
+++ main.rb
@@ -13,31 +13,32 @@ require_relative "console.rb"
 
 # Initialize objects
 board = Board.new
-p1 = PlayerSequential.new
-# p1 = PlayerRandom.new
-# p2 = PlayerSequential.new
-p2 = PlayerRandom.new
+# p1 = PlayerSequential.new  # alternate p1
+p1 = PlayerRandom.new
+p2 = PlayerSequential.new
+# p2 = PlayerRandom.new  # alternate p2
 turn = Turn.new
 position = Position.new
 win = Win.new
 console = Console.new
 
-# Endgame condition checks
+# Endgame condition checks - default to false
 x_won = false
 o_won = false
 full = false
 
 # Each iteration == 1 (attempted) move
 while x_won == false && o_won == false && full == false
-  round = turn.get_round(board.x_count, board.o_count)
-  round % 2 == 0 ? player = p2 : player = p1
-  mark = turn.get_mark(board.x_count, board.o_count)
-  move = player.get_move(board.game_board)
-  location = position.get_index(move)
-  board.set_position(location, mark) if board.position_open?(location)
-  x_won = win.x_won?(board.get_x)
-  o_won = win.o_won?(board.get_o)
-  full = board.board_full?
+  round = turn.get_round(board.x_count, board.o_count)  # puts round  # see the current round number
+  round % 2 == 0 ? player = p2 : player = p1  # puts player  # see which player moved during this turn
+  mark = turn.get_mark(board.x_count, board.o_count)  # puts mark  # see which mark was used
+  move = player.get_move(board.game_board)  # puts move  # see what game_board position was selected
+  location = position.get_index(move)  # puts location  # see the corresponding game_board array index
+  board.set_position(location, mark)
+  x_won = win.x_won?(board.get_x)  # puts x_won  # see if x won (t/f)
+  o_won = win.o_won?(board.get_o)  # puts o_won  # see if o won (t/f)
+  full = board.board_full?  # puts full # see if the game_board is full (t/f)
+  # p board.game_board  # view the game_board array
 end
 
 # Console output for game results (board and status)
