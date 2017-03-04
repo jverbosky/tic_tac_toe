7336ad19045cc42f93cc1ae2415ae7ce71a4c4aa
jverbosk <jverbosk@gmail.com>
2017-03-03 20:41:43 -0500

    added logic to move() to handle non-win situations, unbeatable player ready to test

--- sandbox/3.rb
+++ sandbox/3.rb
@@ -86,24 +86,51 @@ round_8 = ["O", "X", "X", "", "O", "", "X", "O", ""]  # variation 8 - O wins at 
 # Board positions after round 8
 # --------------------------------------------------------------------
 # X O X                        # O X X
-# - O O   # variation 1        # - O -   # variation 8 (nothing to block)
+# - O -   # variation 1        # - O -   # variation 8 (nothing to block)
 # O X X                        # X O O
 # --------------------------------------------------------------------
 # Block when 3 potentially winning positions (2 already taken by O)
-# round_9 = ["X", "O", "X", "", "O", "", "O", "X", "X"]  # variation 1 - X blocks O at 
-# round_9 = ["X", "", "O", "O", "O", "X", "X", "", "X"]  # variation 2 - X blocks O at 
-# round_9 = ["X", "", "X", "X", "O", "O", "O", "", "X"]  # variation 3 - X blocks O at 
-# round_9 = ["X", "X", "O", "", "O", "", "X", "O", "X"]  # variation 4 - X blocks O at 
-# round_9 = ["X", "O", "X", "", "O", "", "X", "X", "O"]  # variation 5 - X blocks O at 
-# round_9 = ["X", "", "X", "O", "O", "X", "X", "", "O"]  # variation 6 - X blocks O at 
-# round_9 = ["O", "", "X", "X", "O", "O", "X", "", "X"]  # variation 7 - X blocks O at 
+round_9 = ["X", "O", "X", "", "O", "", "O", "X", "X"]  # variation 1 - X wins at 5
+# round_9 = ["X", "", "O", "O", "O", "X", "X", "", "X"]  # variation 2 - X wins at 7
+# round_9 = ["X", "", "X", "X", "O", "O", "O", "", "X"]  # variation 3 - X wins at 1
+# round_9 = ["X", "X", "O", "", "O", "", "X", "O", "X"]  # variation 4 - X wins at 3
+# round_9 = ["X", "O", "X", "", "O", "", "X", "X", "O"]  # variation 5 - X wins at 3
+# round_9 = ["X", "", "X", "O", "O", "X", "X", "", "O"]  # variation 6 - X wins at 1
+# round_9 = ["O", "", "X", "X", "O", "O", "X", "", "X"]  # variation 7 - X wins at 7
 # round_9 = ["O", "X", "X", "", "O", "", "X", "O", "O"]  # variation 8 - game over
+# --------------------------------------------------------------------
+# Multiple possible blocks
+# --------------------------------------------------------------------
+# O O -
+# X O X
+# X - -
+# --------------------------------------------------------------------
+round_b = ["O", "O", "", "X", "O", "X", "X", "", ""]   # X blocks O at 2, 7 or 8
+# --------------------------------------------------------------------
+# Multiple possible wins
+# --------------------------------------------------------------------
+# X X -
+# O X O
+# O - -
+# --------------------------------------------------------------------
+round_w = ["X", "X", "", "O", "X", "O", "O", "", ""]   # X wins at 2, 7 or 8
+# --------------------------------------------------------------------
+# Multiple possible moves (non-winning/non-blocking)
+# - develop for non-perfect opening moves
+# - also needed for endgame
+# --------------------------------------------------------------------
+# X - -                        # O X -
+# - - O   # variation 1        # - X O   # variation 2 (non-perfect round 7)
+# O X -                        # - O X
+# --------------------------------------------------------------------
+# round_n = ["X", "", "", "", "", "O", "O", "X", ""]   # variation 1 - X will take 1, 2, 3, 4 or 8
+round_n = ["O", "X", "", "", "X", "O", "", "O", "X"]   # variation 2 - X will take 2, 3 or 6
 # --------------------------------------------------------------------
 
 wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
 board = Board.new
-board.game_board = round_4  # change round number to test different board layouts
+board.game_board = round_n  # change round number to test different board layouts
 
 x_pos = board.get_x
 o_pos = board.get_o
@@ -114,8 +141,8 @@ o_pos = board.get_o
 def opening_x(round, x_pos)
   position = false
   case round
-    when 1 then position = [0, 2, 6, 8].sample
-    when 3 then
+    when 1 then position = [0, 2, 6, 8].sample  # take a corner
+    when 3 then  # take the opposite corner
       case x_pos
         when [0] then position = 8
         when [2] then position = 6
@@ -130,8 +157,8 @@ end
 def opening_o(round)
   position = false
   case round
-    when 2 then position = 4
-    when 4 then position = [1, 3, 5, 7].sample
+    when 2 then position = 4  # take the center
+    when 4 then position = [1, 3, 5, 7].sample  # take an edge
   end
   position
 end
@@ -145,7 +172,10 @@ def move(wins, player, opponent)
       position.push(difference[0]) unless (opponent & difference).count == 1
     end
   end
-  position.count > 0 ? position[0] : false
+  if position.count == 0  # if nothing to block or win, randomly collect an open position
+    position.push((Array(0..8) - (player + opponent)).sample)
+  end
+  position.sample  # .sample in case of multiple
 end
 
 # Method to return position to block, call win() if nothing to block
@@ -157,11 +187,11 @@ def block(wins, player, opponent)
       position.push(difference[0]) unless (player & difference).count == 1
     end
   end
-  position.count > 0 ? position[0] : move(wins, player, opponent)
+  position.count > 0 ? position.sample : move(wins, player, opponent)  # .sample in case of multiple
 end
 
 # p block(wins, o_pos, x_pos)  # O blocking X (round 6 - working, round 8 - working)
-# p block(wins, x_pos, o_pos)  # X blocking O (round 5 - working, round 7 - working, round 9 - )
+p block(wins, x_pos, o_pos)  # X blocking O (round 5 - working, round 7 - working, round 9 - )
 
 # --------------------------------------------------------------------
 # Historical Block Variations (here for reference)
