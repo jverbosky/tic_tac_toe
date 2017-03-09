eab6c50ba3602c0e990ca64dc71146dc55fdaed6
jverbosk <jverbosk@gmail.com>
2017-03-09 00:04:32 -0500

    added logic to display game board for each round and require user keypress to advance to next round

--- console.rb
+++ console.rb
@@ -3,20 +3,24 @@ require_relative "board.rb"
 # class to outputting final game board and results to terminal
 class Console
 
-  def initialize
-    puts "-" * 11
-    puts "Tic Tac Toe"
-    puts "-" * 11
+  # Method to clear the screen regardless of OS
+  def clear_screen
+    RUBY_PLATFORM =~ /cygwin|mswin|mingw|bccwin|wince|emx/ ? system("cls") : system("clear")
   end
 
   def output_board(board)
-    board.map! { |mark| mark == "" ? " " : mark }
-    rows = board.each_slice(3).to_a
+    clear_screen
+    puts "-" * 11
+    puts "Tic Tac Toe"
+    puts "-" * 11
+    spaced = []
+    board.each { |mark| mark == "" ? spaced.push(" ") : spaced.push(mark) }
+    rows = spaced.each_slice(3).to_a
     rows.each { |row|  print "  " + row.join(" ") + "\n" }
+    puts "-" * 11
   end
 
   def output_results(x_won, o_won)
-    puts "-" * 11
     if x_won == true
       puts "Player 1 (X) won!"
     elsif o_won == true
@@ -31,11 +35,11 @@ class Console
 end
 
 # Sandbox testing
-board = Board.new
-console = Console.new
+# board = Board.new
+# console = Console.new
 # board.game_board = ["", "", "", "", "X", "", "", "", ""]  # O
 # board.game_board = ["", "", "", "", "X", "", "O", "", ""]  # X
 # board.game_board = ["O", "X", "", "", "O", "X", "X", "", "O"]  # X
-board.game_board = ["O", "X", "X", "", "O", "X", "X", "", "O"]  # O
-console.output_board(board.game_board)
-console.output_results(x_won, o_won)
+# board.game_board = ["O", "X", "X", "", "O", "X", "X", "", "O"]  # O
+# console.output_board(board.game_board)
+# console.output_results(x_won, o_won)
--- main.rb
+++ main.rb
@@ -66,29 +66,31 @@ full = false
 #
 # Each iteration == 1 (attempted) move
 while x_won == false && o_won == false && full == false
+  console.output_board(board.game_board)
+  print "Press Enter to view the next round..."
+  next_move = gets.chomp
   round = board.get_round(board.x_count, board.o_count)  # puts round  # see the current round number
-  # puts round  # see the current round number
+  puts round  # see the current round number
   round % 2 == 0 ? player = p2 : player = p1  # puts player  # see which player moved during this turn
-  # puts player  # see which player moved during this turn
+  puts player  # see which player moved during this turn
   mark = board.get_mark(board.x_count, board.o_count)  # puts mark  # see which mark was used
-  # puts mark  # see which mark was used
+  puts mark  # see which mark was used
   x_pos = board.get_x
   o_pos = board.get_o
   move = player.get_move(board.game_board, round, mark, wins, x_pos, o_pos)  # puts move  # see what game_board position was selected
-  # puts move  # see what game_board position was selected
+  puts move  # see what game_board position was selected
   location = position.get_index(move)  # puts location  # see the corresponding game_board array index
-  # puts location  # see the corresponding game_board array index
+  puts location  # see the corresponding game_board array index
   board.set_position(location, mark)
   x_won = board.x_won?(board.get_x)  # puts x_won  # see if x won (t/f)
-  # puts x_won  # see if x won (t/f)
+  puts x_won  # see if x won (t/f)
   o_won = board.o_won?(board.get_o)  # puts o_won  # see if o won (t/f)
-  # puts o_won  # see if o won (t/f)
+  puts o_won  # see if o won (t/f)
   full = board.board_full?  # puts full # see if the game_board is full (t/f)
-  # puts full # see if the game_board is full (t/f)
+  puts full # see if the game_board is full (t/f)
   # p board.game_board  # view the game_board array
-  # p board.game_board  # view the game_board array
+  p board.game_board  # view the game_board array
 end
-
 
 # Console output for game results (board and status)
 console.output_board(board.game_board)
