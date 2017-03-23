4ca923f3faa34a493384d6e78740142699ed89b8
jverbosk <jverbosk@gmail.com>
2017-03-23 12:01:33 -0400

    pure assertsions, removed session-related items, commented out clarification statements in test_post_players

--- tests/test_app.rb
+++ tests/test_app.rb
@@ -11,46 +11,6 @@ class TestApp < Minitest::Test  # TestApp subclass inherits from Minitest::Test 
     TicTacToeApp  # most examples use App.new - reason why we don't need .new here?     ?????
   end
 
-  class SessionData
-    def initialize(cookies)
-      @cookies = cookies
-      @data = cookies['rack.session']
-      if @data
-        @data = @data.unpack("m*").first
-        @data = Marshal.load(@data)
-      else
-        @data = {}
-      end
-    end
-
-    def [](key)
-      @data[key]
-    end
-
-    def []=(key, value)
-      @data[key] = value
-      session_data = Marshal.dump(@data)
-      session_data = [session_data].pack("m*")
-      @cookies.merge("rack.session=#{Rack::Utils.escape(session_data)}", URI.parse("//example.org//"))
-      raise "session variable not set" unless @cookies['rack.session'] == session_data
-    end
-  end
-
-  def session
-    SessionData.new(rack_test_session.instance_variable_get(:@rack_mock_session).cookie_jar)
-  end
-
-  # def example_of_using_and_accessing_session_variables
-  #   session[:game] = Game.new
-  #   player_type = {"p1_type"=>"Perfect", "p2_type"=>"Human"}
-  #   session[:game].select_players(player_type)  # initialize player objects based on player_type hash
-  #   session[:p1_type] = session[:game].p1_type  # assign p1_type session to @p1_type in game.rb
-  #   session[:p2_type] = session[:game].p2_type  # assign p1_type session to @p2_type in game.rb
-  #   route = (session[:p1_type] == "Human") ? "/play_human" : "/play_ai"
-  #   puts "route: #{route}"
-  #   puts "round: #{session[:game].round}"
-  # end
-
   def test_get_entry_page
     get '/'  # verify a (get '/' do) route exists - doesn't need erb statement to pass
     assert(last_response.ok?)  # verify server response == 200 for (get '/') action - doesn't need erb statement to pass
@@ -66,8 +26,8 @@ class TestApp < Minitest::Test  # TestApp subclass inherits from Minitest::Test 
 
   def test_post_players
     post '/players', player_type: {"p1_type"=>"Perfect", "p2_type"=>"Human"}
-    output = last_response.to_a  # use to put last_response object data in an array
-    puts "last response: #{output}"  # use to make last_response data visible for seeing errors
+    # output = last_response.to_a  # use to put last_response object data in an array
+    # puts "last response: #{output}"  # use to make last_response data visible for seeing errors
     assert(last_response.ok?)
     assert(last_response.body.include?('Got it!'))
     assert(last_response.body.include?('Perfect'))
